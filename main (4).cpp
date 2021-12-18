1349a0486570ad990a80a61864b678486425c22f
Andrew Coviello <78008163+Vell-ow@users.noreply.github.com>
2021-12-17 21:31:56 -0800

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,9 @@
+// ICO2_Part4: Write and display your favorite haiku to the screen.
+// Author: Andrew Coviello
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "My favorite one \n" << "Is not in fact publicized \n" << "Because this one is it" << endl;
+
+}
